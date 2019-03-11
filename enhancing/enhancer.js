c7cff44e5d9f793b80a658daae1986c129ac8fa3
matt0418 <mfeldma7@gmail.com>
2019-03-11 16:49:21 -0400

    completed writing function for succeed and fail and their respective tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,12 +6,34 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newItem = {...item}
+    ++newItem.enhancement
+    return newItem
+  } else {
+    const newItem = {...item}
+    return newItem
+  }
 }
 
 function fail(item) {
-  return { ...item };
-}
+  if (item.enhancement <  15) {
+    const newItem = {...item}
+    newItem.durability = newItem.durability-5
+    return newItem
+  } else {
+    if (item.enhancement > 16) {
+      newItem = {...item}
+      newItem.enhancement = newItem.enhancement-1
+      newItem.durability = newItem.durability-10
+      return newItem
+    } else {
+      newItem = {...item}
+      newItem.durability = newItem.durability-10
+      return newItem
+    }
+  }
+ }
 
 function repair(item) {
   return { ...item };
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,57 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail } = require('./enhancer.js');
 // test away!
+let itemHighEnhacement = {
+    name: "test",
+    enhancement: 20,
+    durability: 10
+}
+
+let item = {
+    name: "test",
+    enhancement: 14,
+    durability: 10
+}
+
+let newItem = {
+    name: "test",
+    enhancement: 15,
+    durability: 10
+}
+
+let failItem = {
+    name: "test",
+    enhancement: 14,
+    durability: 12
+}
+
+
+let failNewItem = {
+    name: "test",
+    enhancement: 14,
+    durability: 10
+}
+
+
+describe('enhancer.js', () => {
+    describe('succeed()', () => {
+        it('should return newItem same enhancement', () => {
+            expect(succeed(itemHighEnhacement)).toEqual(itemHighEnhacement)
+        })
+
+        it('should return newItem with higher enhacement', () => {
+            expect(succeed(item)).toEqual(newItem)
+        })
+    })
+
+    describe('fail()', () => {
+        it('should return new item with durability decreased by 5', () => {
+            expect(fail({name: "test", enhancement: 14, durability: 10})).toEqual({name: "test", enhancement: 14, durability: 5})
+        })
+        it('should return new item with durability decreased by 10', () => {
+            expect(fail({name: "test", enhancement: 15, durability: 12})).toEqual({name: "test", enhancement: 15, durability: 2})
+        })
+        it('should return new item with durability decreased by 10 and enhacnement decreased by 1', () => {
+            expect(fail({name: "test", enhancement: 18, durability: 12})).toEqual({name: "test", enhancement: 17, durability: 2})
+        })
+    })
+})
